79a03259641af27a15c25a44421dca56961daf1f
tianhao139982 <tianhao_sz@aliyun.com>
2019-07-18 09:52:23 +0800

    add projrct file

---
+++ src/pages/About/About.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <h1>关于</h1>
+        <hr>
+        <div>
+            <div>
+                <span>
+                    <!-- <i class="iconfont icon-github"></i> -->
+                    <h4>欢迎大家对此项目提出改进意见 qq:251722976</h4>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "About"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container-fluid py-5 home"  lazy="loaded">
+        <div class="row justify-content-center py-5">
+            <div class="text-center col-md-12">
+                <div class="title">E-Book</div>
+                <div class="subtitle mb-5">一个沉浸交互式新风格线上书店</div>
+                <div class="text">你所想要的 都是我们所努力的</div>
+            </div>
+            <div class="mt-4">
+                <button class="btn btn-primary" @click="$router.push('/books')">立即开始探索</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        data() {
+            return {
+                imgUrl: '../../../public/images/home.png'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .title{
+        font-family: Calibri;
+        font: 72px bold;
+    }
+
+    .subtitle{
+        font-size: 16px;
+        font-family: 等线;
+    }
+
+    .text{
+        font-size: 20px;
+    }
+
+    .home[lazy=loaded]{
+        height: 600px;
+        background:url('../../../public/images/home.png');
+        background-size:cover;
+    }
+</style>
